b844e7ec80cb4f01b5dbc118b2d074733dcead02
Zachary0216 <68837452+Zachary0216@users.noreply.github.com>
2021-10-28 11:26:59 +0800

    fix +test

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php session_start(); ?>
 <?php $title = "PetterTogether - Home Page";?>
 <?php require_once "./components/header.php"; ?>
-<?php unset($_SESSION['payment']); ?>
+<?php if (isset($_SESSION['payment']))
+    {unset($_SESSION['payment']);
+    } ?>
 <!-- Main Section - Navbar & Hero Section -->
 <section class="main-section">
     <div class="background-filter">
